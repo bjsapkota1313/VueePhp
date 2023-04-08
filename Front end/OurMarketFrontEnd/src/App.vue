094b424d7bd24d18f33f0423b8b6067eb041319a
bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
2023-04-08 12:17:45 +0200

    dfdf
    dfdf

--- Front end/OurMarketFrontEnd/src/App.vue
+++ Front end/OurMarketFrontEnd/src/App.vue
@@ -7,9 +7,9 @@
   <Footer v-if="$route.path !== '/login' && $route.path !== '/login/signup'" ></Footer>
 </template>
 <script >
-import { useUserSessionStore } from './stores/userSession.js'
+import { useUserSessionStore } from '@/stores/userSession.js'
 import { RouterLink, RouterView } from 'vue-router';
-import Navigation from './components/Navigation.vue'
+import Navigation from '@/components/Navigation.vue';
 import {UseShoppingCartStore} from "@/stores/ShoppingCart";
 import Footer from "@/components/Footer.vue";
 
